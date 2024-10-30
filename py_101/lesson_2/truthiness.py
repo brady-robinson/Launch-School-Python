800d6dd56986bb61db9d27679f6e69d5b298a1e0
Brady Robinson <bradyrobinson3@gmail.com>
2024-10-30 05:38:15 -0400

    add file

---
+++ py_101/lesson_2/truthiness.py
@@ -0,0 +1,34 @@
+# print(True)
+# print(False)
+
+# def make_longer(string, longer):
+#     if longer:
+#         return string + string 
+#     else:
+#         return string
+
+# print(make_longer("abc", True))
+# print(make_longer("xyz", False))
+
+# def is_digit(char):
+#     if '0' <= char <= '9':
+#         return True
+#     else:
+#         return False
+
+# print(is_digit('5'))
+# print(is_digit('a'))
+
+# value = True
+# if value is True:
+#     print("It's True")
+# elif value is False:
+#     print("It's false!")
+# else:
+#     print("It's not true of false!")
+
+num = 5
+if num < 10:
+    print("small number")
+else:
+    print("large number")
