f64004635985a9c561cfca8781197dbe2127c26d
Brady Robinson <bradyrobinson3@gmail.com>
2025-01-23 06:42:21 -0500

    add files

--- todolist/todo.py
+++ todolist/todo.py
@@ -1,66 +1,29 @@
 class Todo:
-    def __init__(self, name, done=False):
-        self.name = name
-        self.done = done
+    IS_DONE = 'X'
+    IS_UNDONE = ' '
+
+    def __init__(self, title):
+        self._title = title
+        self._done = False
 
     @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, new_name):
-        self._name = new_name
+    def title(self):
+        return self._title
 
     @property
     def done(self):
         return self._done
 
     @done.setter
-    def done(self, status):
-        if not isinstance(status, bool):
-            raise TypeError
-
-        self._done = status
+    def done(self, done):
+        self._done = done
 
     def __str__(self):
-        if self.done == False:
-            mark = ' '
-        else:
-            mark = 'X'
-
-        return f'[{mark}] {self.name}'
+        marker = self.IS_DONE if self.done else self.IS_UNDONE
+        return f'[{marker}] {self.title}'
 
     def __eq__(self, other):
-        return self.name == other.name and self.done == other.done
+        if isinstance(other, Todo):
+            return self.title == other.title and self.done == other.done
 
-def test_todo():
-    todo1 = Todo('Buy milk')
-    todo2 = Todo('Clean room')
-    todo3 = Todo('Go to gym')
-    todo4 = Todo('Clean room')
-
-    print(todo1)                  # [ ] Buy milk
-    print(todo2)                  # [ ] Clean room
-    print(todo3)                  # [ ] Go to gym
-    print(todo4)                  # [ ] Clean room
-
-    print(todo2 == todo4)         # True
-    print(todo1 == todo2)         # False
-    print(todo4.done)             # False
-
-    todo1.done = True
-    todo4.done = True
-    print(todo4.done)             # True
-
-    print(todo1)                  # [X] Buy milk
-    print(todo2)                  # [ ] Clean room
-    print(todo3)                  # [ ] Go to gym
-    print(todo4)                  # [X] Clean room
-
-    print(todo2 == todo4)         # False
-
-    todo4.done = False
-    print(todo4.done)             # False
-    print(todo4)                  # [ ] Clean room
-
-test_todo()
+        return NotImplemented
